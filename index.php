3687ca0729d1007084ebfa054b24361005c0fe3e
shivaram0583 <46864551+shivaram0583@users.noreply.github.com>
2019-10-15 09:26:42 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "id10198969_shivaram0583";
+$password = "1081956";
+$dbname = "id10198969_vitap";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE 2019BATCH (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
